ff05c1c2f3908df403fcb386c88d825c72ff152d
Horizonnns <for.office.gthb@gmail.com>
2024-03-26 01:09:08 +0500

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<script setup>
+import Calendar from './components/Calendar.vue';
+</script>
+
+<template>
+	<section class="w-full h-screen flex items-center justify-center">
+		<Calendar />
+	</section>
+</template>
