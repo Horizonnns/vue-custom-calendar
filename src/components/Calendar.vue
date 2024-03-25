bf69ac431ca9fa600b98df687da848093498e189
Horizonnns <for.office.gthb@gmail.com>
2024-03-26 01:19:44 +0500

    Added variables containing all months

--- src/components/Calendar.vue
+++ src/components/Calendar.vue
@@ -1,4 +1,36 @@
-<script setup></script>
+<script setup>
+// monthes
+const MONTHSRU = [
+	'Январь',
+	'Февраль',
+	'Март',
+	'Апрель',
+	'Май',
+	'Июнь',
+	'Июль',
+	'Август',
+	'Сентябрь',
+	'Октябрь',
+	'Ноябрь',
+	'Декабрь',
+];
+
+const MONTHSEN = [
+	'January',
+	'February',
+	'March',
+	'April',
+	'May',
+	'June',
+	'July',
+	'August',
+	'September',
+	'October',
+	'November',
+	'December',
+];
+
+</script>
 
 <template>
 	<p>Calendar</p>
